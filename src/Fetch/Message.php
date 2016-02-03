3b4ca0c356097aeb3657c9172ed4c698354d39b5
Sean Sabbage <sgsabbage@gmail.com>
2016-02-03 10:35:32 +0000

    Fixes case sensitivity issue with using mb_list_encodings()

--- src/Fetch/Message.php
+++ src/Fetch/Message.php
@@ -523,7 +523,9 @@ class Message
             if (!empty($parameters['charset']) && $parameters['charset'] !== self::$charset) {
                 $mb_converted = false;
                 if (function_exists('mb_convert_encoding')) {
-                    if (!in_array($parameters['charset'], mb_list_encodings())) {
+                    $encs = mb_list_encodings();
+                    $encsLower = array_map( 'strtolower', $encs );
+                    if (!in_array(strtolower($parameters['charset']), $encsLower)) {
                         if ($structure->encoding === 0) {
                             $parameters['charset'] = 'US-ASCII';
                         } else {
